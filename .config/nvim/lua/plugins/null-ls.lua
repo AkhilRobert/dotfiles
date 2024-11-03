d64db75353ac0b678847409ca71dc59b9edd11ca
Akhil Robert <akhilrobert2412@gmail.com>
2024-11-03 11:43:08 +0530

    update

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -50,14 +50,14 @@ return {
       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
       return function(_, bufnr)
         -- Autocmd for formatting on save
-        -- vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        -- vim.api.nvim_create_autocmd("BufWritePre", {
-        --   group = augroup,
-        --   buffer = bufnr,
-        --   callback = function()
-        --     vim.lsp.buf.format({ bufnr = bufnr })
-        --   end,
-        -- })
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
       end
     end
 
